e152bc960ae68d0f151a2ab5622b430eeb41273c
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2017-09-26 23:11:23 +0200

    Refactor history_service

--- app/services/history_service.rb
+++ app/services/history_service.rb
@@ -1,8 +1,6 @@
 class HistoryService
-  API_URL = 'http://history.muffinlabs.com/date'.freeze
-
   def self.load_random(url)
-    json_response = HTTP.get("#{API_URL}/#{url}").parse(:json)
+    json_response = HTTP.get("#{ENV['HISTORY_API_URL']}/#{url}").parse(:json)
 
     json_response['data']['Events'].sample
   end
